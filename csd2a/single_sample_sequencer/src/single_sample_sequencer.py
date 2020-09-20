51efde6f535395da0d10519b6acf3d574f7455e8
Mark Smits <marksmits2010@live.nl>
2020-09-20 11:51:52 +0200

    tweede commit, timestamps naar secondes omgerekend

--- csd2a/single_sample_sequencer/src/single_sample_sequencer.py
+++ csd2a/single_sample_sequencer/src/single_sample_sequencer.py
@@ -19,9 +19,21 @@ else:
 # functie die kwartnootintervallen omzet naar 16e timestamps
 noteDurations = [0.25, 0.5, 1]
 timestamps16th = [0]
+sixteenthNoteDuration = 15/bpm
 def durationsToTimestamps16th(noteDurs):
     for i, n in enumerate(noteDurs):
-        timestamps16th.append(timestamps16th[i] + noteDurs[i] * 4)
+        if i == len(noteDurs) - 1:
+            print("check")
+        else:
+            timestamps16th.append(timestamps16th[i] + n * 4)
 
 durationsToTimestamps16th(noteDurations)
 print(timestamps16th)
+
+timestampssecs = []
+def timestamps16thToTimestampssecs(ts16th):
+    for n in ts16th:
+        timestampssecs.append(n * sixteenthNoteDuration)
+
+timestamps16thToTimestampssecs(timestamps16th)
+print(timestampssecs)
