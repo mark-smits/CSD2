0dbe9535d0405004a94e4cd482f6d51358e25e91
Mark Smits <marksmits2010@live.nl>
2020-09-20 12:09:32 +0200

    derde commit, audio en sampleplayback toegevoegd

--- csd2a/single_sample_sequencer/src/single_sample_sequencer.py
+++ csd2a/single_sample_sequencer/src/single_sample_sequencer.py
@@ -10,8 +10,6 @@ userbpm = int(input("change bpm?"))
 # check user bpm input
 if userbpm <= 0:
     print("bpm too low")
-#elif userbpm == None:
-#    print("using default bpm: " + str(bpm))
 else:
     bpm = userbpm
     print("using bpm: " + str(bpm))
@@ -19,7 +17,6 @@ else:
 # functie die kwartnootintervallen omzet naar 16e timestamps
 noteDurations = [0.25, 0.5, 1]
 timestamps16th = [0]
-sixteenthNoteDuration = 15/bpm
 def durationsToTimestamps16th(noteDurs):
     for i, n in enumerate(noteDurs):
         if i == len(noteDurs) - 1:
@@ -30,10 +27,27 @@ def durationsToTimestamps16th(noteDurs):
 durationsToTimestamps16th(noteDurations)
 print(timestamps16th)
 
+# functie die zestiende timestamps omzet naar secondes
 timestampssecs = []
-def timestamps16thToTimestampssecs(ts16th):
+def timestamps16thToTimestampssecs(ts16th, tempo):
     for n in ts16th:
-        timestampssecs.append(n * sixteenthNoteDuration)
+        timestampssecs.append(n * 15/tempo)
 
-timestamps16thToTimestampssecs(timestamps16th)
+timestamps16thToTimestampssecs(timestamps16th, bpm)
+finalsleep = noteDurations[len(noteDurations)-1]*60/bpm
 print(timestampssecs)
+
+# laad audio in
+wave_obj = sa.WaveObject.from_wave_file(r"..\audio\Chord.wav")
+
+# functie die samples in een sequences afspeelt
+t0 = time.time()
+timestamp = timestampssecs.pop(0)
+while True:
+    if time.time() >= t0 + timestamp:
+        wave_obj.play()
+        if timestampssecs:
+            timestamp = timestampssecs.pop(0)
+        else:
+            time.sleep(finalsleep)
+            break
