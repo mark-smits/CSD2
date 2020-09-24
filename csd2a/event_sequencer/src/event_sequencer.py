8ec6f36a9de3e58c2a33d1047191739cdd28df83
Mark Smits <marksmits2010@live.nl>
2020-09-24 10:10:24 +0200

    commit 2, ritme word gegenereerd en gesorteerd

--- csd2a/event_sequencer/src/event_sequencer.py
+++ csd2a/event_sequencer/src/event_sequencer.py
@@ -6,21 +6,40 @@ kick = sa.WaveObject.from_wave_file("../audio/Kick.wav")
 snare = sa.WaveObject.from_wave_file("../audio/Snare.wav")
 clap = sa.WaveObject.from_wave_file("../audio/Clap.wav")
 hhopen = sa.WaveObject.from_wave_file("../audio/HHOpen.wav")
-hhclose = sa.WaveObject.from_wave_file("../audio/HHClosed.wav")
+hhclosed = sa.WaveObject.from_wave_file("../audio/HHClosed.wav")
 
 # event maker
-def make_event(stamp, inst, instname, vel):
+def make_event(stamp, inst, instname):
     event = {
         'timestamp': stamp,
         'instrument': inst,
-        'instrumentname': instname,
-        'velocity': vel,
-        'duration': 200
+        'instrumentname': instname
         }
     return event
 
-# test case
-snare_event = make_event(100, snare, "snare", 100)
-snare_event['instrument'].play()
-time.sleep(1)
-print(snare_event)
+# ritme genereren
+bpm = int(input("BPM?"))
+sixteenth = 15/bpm
+ritme = []
+    # claps
+ritme.append(make_event(4*sixteenth, clap, "clap"))
+ritme.append(make_event(12*sixteenth, clap, "clap"))
+    # snare
+ritme.append(make_event(15*sixteenth, snare, "snare"))
+    # kick
+for i in range(3):
+    ritme.append(make_event(i*4*sixteenth, kick, "kick"))
+    # hats
+    ritme.append(make_event((i*4+2)*sixteenth, hhopen, "hhopen"))
+for i in range(15):
+    ritme.append(make_event(i*sixteenth, hhclosed, "hhclosed"))
+
+def keygen(e):
+    return e['timestamp']
+
+print(ritme)
+ritme.sort(key=keygen)
+print(ritme)
+
+
+# event handler
